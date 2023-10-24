51957a3a03ce9563dc0e6522a55c54562a4bae51
thalesdemo <hello@onewelco.me>
2023-10-24 00:56:10 -0700

    enhanced logging and network filter logic

--- src/main/java/com/thalesdemo/safenet/auth/api/IpAndHeaderBasedFilter.java
+++ src/main/java/com/thalesdemo/safenet/auth/api/IpAndHeaderBasedFilter.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.*;
@@ -14,20 +15,22 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  * This filter enforces IP and X-Forwarded-For header based filtering.
  * It uses the configuration provided to check if incoming requests should be
  * allowed or not.
- * 
+ *
  * IP filtering checks the actual IP of the incoming request while
  * X-Forwarded-For filtering checks the IP provided in the X-Forwarded-For
  * header.
- * 
- * The filter also ensures it doesn't process the same request multiple times
- * by setting and checking a request attribute.
- * 
+ *
+ * Requests are logged with the following format:
+ * <HTTP METHOD> | Remote-IP: REMOTE-IP | X-Forwarded-For: X-FORWARDED-FOR-IP |
+ * URL: URL | rc=RETURN CODE
+ *
  * @author YourName
  */
 @Component
@@ -39,13 +42,15 @@ public class IpAndHeaderBasedFilter implements Filter {
 
     private List<IpAddressMatcher> allowedIpMatchers;
     private List<IpAddressMatcher> allowedXForwardedMatchers;
+    private static final String FILTER_PROCESSED_REQUEST_ATTR = "FILTER_PROCESSED_REQUEST";
 
     // Logger instance
     private static final Logger logger = Logger.getLogger(IpAndHeaderBasedFilter.class.getName());
 
-    // Attribute to ensure request is processed only once
-    private static final String FILTER_PROCESSED_REQUEST_ATTR = "IpAndHeaderBasedFilter.processed";
-
+    /**
+     * Initialize the allowed IP matchers for both direct IP
+     * and X-Forwarded-For header checks after bean properties are set.
+     */
     @PostConstruct
     public void initSubnets() {
         this.allowedIpMatchers = config.getAllowedIpRanges().stream()
@@ -56,69 +61,167 @@ public class IpAndHeaderBasedFilter implements Filter {
                 .map(IpAddressMatcher::new)
                 .collect(Collectors.toList());
 
-        logger.info("Network Config Initialized: " + config.toString());
+        logger.log(Level.INFO, "Network Config {0}", config.toString());
     }
 
+    /**
+     * Called by the web container to indicate to a filter that it
+     * is being placed into service. No specific logic here for now.
+     *
+     * @param filterConfig the filter configuration object.
+     * @throws ServletException in case of an initialization failure.
+     */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
+    /**
+     * The main method of the filter where the logic to check the IP address
+     * and the X-Forwarded-For header is applied. If the request doesn't match
+     * the allowed addresses or headers, it returns a forbidden error.
+     * All requests are also logged with the desired format.
+     *
+     * @param request  the servlet request.
+     * @param response the servlet response.
+     * @param chain    the filter chain.
+     * @throws IOException      in case of an I/O error.
+     * @throws ServletException in case of a general service failure.
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        // Check if this filter already processed the request
+        // Check if the request has already been processed by this filter
         if (httpRequest.getAttribute(FILTER_PROCESSED_REQUEST_ATTR) != null) {
             chain.doFilter(request, response);
             return;
         }
 
-        logger.info("Headers: " + Collections.list(httpRequest.getHeaderNames()).stream()
-                .map(headerName -> headerName + ": " + httpRequest.getHeader(headerName))
-                .collect(Collectors.joining(", ")));
+        // Use a custom response wrapper to capture the status code for logging
+        StatusResponseWrapper responseWrapper = new StatusResponseWrapper(httpResponse);
 
+        // Check if IP-based filtering is enabled and process accordingly
         if (config.isEnableIpBasedFiltering()) {
             String remoteIp = httpRequest.getRemoteAddr();
-            logger.info("Checking IP: " + remoteIp + " for path: " + httpRequest.getRequestURI());
             if (!isIpAllowed(remoteIp, allowedIpMatchers)) {
-                httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "Forbidden IP");
+                setErrorResponse(httpResponse, HttpStatus.FORBIDDEN.value(), "Forbidden IP");
+                logRequestInfo(httpRequest, responseWrapper);
                 return;
             }
         }
 
+        // Check if X-Forwarded-For header filtering is enabled and process accordingly
         if (config.isEnableXForwardedForFiltering()) {
             String xForwardedFor = httpRequest.getHeader("X-Forwarded-For");
             if (xForwardedFor != null) {
-                logger.info("Checking X-Forwarded-For IP: " + xForwardedFor.split(",")[0].trim());
                 if (!isIpAllowed(xForwardedFor.split(",")[0].trim(), allowedXForwardedMatchers)) {
-                    httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "Forbidden X-Forwarded-For IP");
+                    setErrorResponse(httpResponse, HttpStatus.FORBIDDEN.value(), "Forbidden X-Forwarded-For IP");
+                    logRequestInfo(httpRequest, responseWrapper);
                     return;
                 }
             } else if (config.isStrictEnforcementXForwardedForFiltering()) {
-                httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "Missing X-Forwarded-For Header");
+                setErrorResponse(httpResponse, HttpStatus.FORBIDDEN.value(), "Missing X-Forwarded-For Header");
+                logRequestInfo(httpRequest, responseWrapper);
                 return;
             }
         }
 
-        // Mark this request as processed by this filter
+        // Mark the request as processed by this filter before proceeding
         httpRequest.setAttribute(FILTER_PROCESSED_REQUEST_ATTR, true);
 
-        chain.doFilter(request, response);
+        // Continue with the filter chain
+        chain.doFilter(request, responseWrapper);
+
+        // Log the request information after processing
+        logRequestInfo(httpRequest, responseWrapper);
     }
 
     /**
-     * Checks if a given IP is allowed based on a list of allowed IP matchers.
+     * Sets an error response for the given HTTP response object.
+     * The response will be in a JSON format: { "error": "provided error message" }
+     *
+     * @param httpResponse The HTTP response object to set the error on.
+     * @param statusCode   The HTTP status code to set for the error.
+     * @param message      The error message to be included in the JSON response.
+     * @throws IOException In case of an I/O error while writing to the response.
+     */
+    private void setErrorResponse(HttpServletResponse httpResponse, int statusCode, String message) throws IOException {
+        httpResponse.setStatus(statusCode);
+        httpResponse.setContentType("application/json");
+        String jsonErrorMessage = String.format("{\"error\": \"%s\"}", message);
+        httpResponse.getWriter().write(jsonErrorMessage);
+        httpResponse.getWriter().flush();
+        httpResponse.getWriter().close();
+    }
+
+    /**
+     * A custom wrapper around HttpServletResponse to capture the status code.
+     */
+    private static class StatusResponseWrapper extends HttpServletResponseWrapper {
+        private int httpStatus;
+
+        public StatusResponseWrapper(HttpServletResponse response) {
+            super(response);
+        }
+
+        @Override
+        public void sendError(int sc) throws IOException {
+            httpStatus = sc;
+            super.sendError(sc);
+        }
+
+        @Override
+        public void sendError(int sc, String msg) throws IOException {
+            httpStatus = sc;
+            super.sendError(sc, msg);
+        }
+
+        @Override
+        public void setStatus(int sc) {
+            httpStatus = sc;
+            super.setStatus(sc);
+        }
+
+        public int getStatus() {
+            return httpStatus;
+        }
+    }
+
+    /**
+     * Checks if a given IP is allowed based on a list of allowed matchers.
      *
      * @param ip              The IP address to check.
-     * @param allowedMatchers A list of allowed IP matchers.
+     * @param allowedMatchers A list of allowed matchers.
      * @return true if the IP is allowed, false otherwise.
      */
     private boolean isIpAllowed(String ip, List<IpAddressMatcher> allowedMatchers) {
         return allowedMatchers.stream().anyMatch(matcher -> matcher.matches(ip));
     }
 
+    /**
+     * Logs the request information in the desired format.
+     *
+     * @param httpRequest     the HTTP request.
+     * @param responseWrapper the wrapped HTTP response to get status.
+     */
+    private void logRequestInfo(HttpServletRequest httpRequest, StatusResponseWrapper responseWrapper) {
+        String xForwardedFor = httpRequest.getHeader("X-Forwarded-For");
+        if (xForwardedFor == null)
+            xForwardedFor = "N/A";
+
+        String logMessage = String.format("%s | Remote-IP: %s | X-Forwarded-For: %s | URL: %s | rc=%d",
+                httpRequest.getMethod(), httpRequest.getRemoteAddr(), xForwardedFor,
+                httpRequest.getRequestURI(), responseWrapper.getStatus());
+
+        logger.info(logMessage);
+    }
+
+    /**
+     * Called by the web container to indicate to a filter that it
+     * is being taken out of service. No specific logic here for now.
+     */
     @Override
     public void destroy() {
     }
--- src/main/java/com/thalesdemo/safenet/auth/api/NetworkFilterConfig.java
+++ src/main/java/com/thalesdemo/safenet/auth/api/NetworkFilterConfig.java
@@ -3,7 +3,6 @@ package com.thalesdemo.safenet.auth.api;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 import lombok.Data;
 
@@ -18,13 +17,4 @@ public class NetworkFilterConfig {
     private boolean strictEnforcementXForwardedForFiltering;
     private List<String> allowedXForwardedForRanges;
 
-    @PostConstruct
-    public void postConstruct() {
-        System.out.println("==== NetworkFilterConfig Initialization ====");
-        System.out.println("Enable IP Based Filtering: " + enableIpBasedFiltering);
-        System.out.println("Allowed IP Ranges: " + allowedIpRanges);
-        System.out.println("Enable X-Forwarded-For Filtering: " + enableXForwardedForFiltering);
-        System.out.println("Strict Enforcement X-Forwarded-For Filtering: " + strictEnforcementXForwardedForFiltering);
-        System.out.println("Allowed X-Forwarded-For Ranges: " + allowedXForwardedForRanges);
-    }
 }
